a45611791807d1bc6a7f99b7263deae842627387
youKeon <yukeon97@gmail.com>
2025-09-24 23:55:07 +0900

    core: project init

---
+++ src/main/java/com/txnow/TxnowApplication.java
@@ -0,0 +1,11 @@
+package com.txnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TxnowApplication.class, args);
+    }
+}
